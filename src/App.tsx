258b8450499a3fac032816129cee2aeab60af360
XtereO <71135845+XtereO@users.noreply.github.com>
2021-07-09 21:11:11 +0300

    pull code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './UI/Header/Header';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Main } from './UI/Main/Main';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Desire } from './UI/Desire/Desire';
+import { Provider } from 'react-redux';
+import {store} from "./BLL/store"
+import { Very } from './UI/Very/Very';
+import { Reason } from './UI/Reason/Reason';
+import { Other } from './UI/Other/Other';
+import { Author } from './UI/Authors/Author';
 
 function App() {
+  const diff=65
+  let [height,setHeight]=useState(window.innerHeight-diff)
+  let [isPC,setMode]=useState(window.innerWidth>760 ? true : false)
+  const setSize=()=>{
+    setHeight(window.innerHeight-diff)
+      if(window.innerWidth>760){
+        setMode(true)
+      }else{
+        setMode(false)
+      }
+  }
+
+  useEffect(()=>{
+    window.addEventListener("resize",setSize)
+    return (()=>{
+      window.removeEventListener("resize",setSize)
+    })
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <HashRouter>
+      <div>
+        <Header/>
+        <div style={{height}} className="body pt-3">
+        <Switch>
+          <Route path="/desire" 
+          render={()=><Desire isPC={isPC} />}/>
+          <Route path="/very" 
+          render={()=><Very isPC={isPC} />}/>
+          <Route path="/reason" 
+          render={()=><Reason isPC={isPC} />}/>
+          <Route path="/other" 
+          render={()=><Other isPC={isPC} />}/>
+          <Route path="/author" 
+          render={()=><Author isPC={isPC} />} />
+          <Route path="/"
+          render={()=><Main height={height}  />} />
+        </Switch>
+        </div>
+      </div>
+    </HashRouter>
+    </Provider>
   );
 }
 
